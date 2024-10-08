2376f113d9aaa6888ed76dfbfc052c0661072757
YAKAN3105 <shanyexiongqing@gmail.com>
2024-10-08 17:02:00 +0900

    up1

--- Hyper Dimensional Tank/Assets/SuginoFile/ResultScore.cs
+++ Hyper Dimensional Tank/Assets/SuginoFile/ResultScore.cs
@@ -8,12 +8,11 @@ public class ResultScore : MonoBehaviour
 {
     public TextMeshProUGUI ScoreText;
     int score;
-
+    public ScoreManager scoreManager;
     // Start is called before the first frame update
     void Start()
     {
-        score = SingleplayWall.getScore();
-
+        score = scoreManager.GetScore();
         ScoreText.text = string.Format("Score:{0}",score);
     }
 
