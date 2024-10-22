2869898fbce63a86796012878057388dd974b1e2
YAKAN3105 <shanyexiongqing@gmail.com>
2024-10-22 18:21:37 +0900

    up

--- Hyper Dimensional Tank/Assets/ren/Main/SinglePlayer.cs
+++ Hyper Dimensional Tank/Assets/ren/Main/SinglePlayer.cs
@@ -11,15 +11,16 @@ using UnityEngine.Windows;
 //using static UnityEditor.Timeline.TimelinePlaybackControls;
 //using static UnityEngine.EventSystems.StandaloneInputModule;
 
-
 public class SinglePlayer : MonoBehaviour
 {
+    [SerializeField] private string playerIndex;
 
-    public string playerIndex;
+    //スタートするまで動けない
+    private StartCount startCount;
+    [SerializeField] private GameObject startCountObj;
 
- 
     //プレイヤーの動き
-    private float moveSpeed = 20f;
+    private float moveSpeed = 12f;
     private float tempSpeed = 0f;
     //private Vector3 moveVec;
     private Vector2 inputMove;
@@ -45,19 +46,24 @@ public class SinglePlayer : MonoBehaviour
 
     //弾の速さ
     private float nomalBulletSpeed = 600f;
-    private float strongBulletSpeed = 400f;
+    private float strongBulletSpeed = 300f;
 
-  
+    //ダメージ
+    private int damegeNomal = 5;
+    private int damegeStrong = 30;
+    private int damegeBeam = 2;
 
+    
    
-
     //爆発
     [SerializeField] private GameObject deadExplosion = null;
 
 
     //玉のクールタイム
-    private int coolTime;
-    private int canCoolTime = 60;
+    private int nomalCoolTime;
+    private int canNomalCoolTime = 25;
+    private int strongCoolTime;
+    private int canStrongCoolTime = 75;
     private bool isShotNomal = true;
     private bool isShotStrong = true;
     //チャージエフェクト
@@ -69,41 +75,57 @@ public class SinglePlayer : MonoBehaviour
     [SerializeField] private GameObject bulletBeam;
     [SerializeField] private GameObject beamCharge;
     private bool isShotBeam = false;
-    private float chargeValue = 0.2f;
+    private float chargeValue = 0.1f;
     public float beamGauge = 0;
     private bool isCharge = false;
     GameObject newBeam = null;
     //ビームの全体フレーム
     private int beamFream = 120;
-    private int beamFreamCount = 0;
+    private float beamFreamCount = 0.0f;
     private bool isBeamCount = false;
 
+    //バリア
+    [SerializeField] private GameObject barrier;
+    private bool isBarrier = false;
+    private float cutRate = 0.5f;
+    private float burrierFreme = 200.0f;
+    public float burrierCoolTime = 1800.0f;
     //InputSystem
     private PlayerControl playerControl;
 
     //SE
-    public AudioClip[] seSound = new AudioClip[2];
+    public AudioClip seSound;
     //public AudioClip beamSound;
     private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        //最初の三秒間は動けない
+        startCount = startCountObj.GetComponent<StartCount>();
+        //エフェクトを非表示
         beamCharge.SetActive(false);
         chargeEffect.SetActive(false);
         maxEffect.SetActive(false);
+        //バリアのクールタイムを0秒に
+        burrierCoolTime = 0.0f;
+        barrier.SetActive(false);
+        //AddForceを使うために取得
         playerRb = this.transform.GetComponent<Rigidbody>();
-        //moveVec = new Vector3(0,0,moveSpeed);
+        //頭を動かせるようにする
         head = transform.GetChild(0);
+        //コントローラーで動かせるようにする
         playerControl = new PlayerControl();
         playerControl.Enable();
+        //弾を打った時にスピードを0にするため一時的にmoveSpeedをtempにいれる
         tempSpeed = moveSpeed;
         //Componentを取得
         audioSource = GetComponent<AudioSource>();
+
     }
     public void HeadRotationLeft(InputAction.CallbackContext context)
     {
+       
         if (context.started) // ボタンを押したとき
         {
             isLeft = true;
@@ -126,8 +148,16 @@ public class SinglePlayer : MonoBehaviour
         }
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (startCount != null)
+        {
+            return;
+        }
+        if(this.transform.position.y <= -5)
+        {
+            this.transform.position = new Vector3(0,5,0);
+        }
       
         /////////////////////////////////////////////////////
         ///ビーム
@@ -139,6 +169,7 @@ public class SinglePlayer : MonoBehaviour
             Destroy(newBeam, 1); //弾を消す
             return;
         }
+
         //ビームのための間頭は動かせる
         if (isLeft)
         {
@@ -150,14 +181,38 @@ public class SinglePlayer : MonoBehaviour
             //Quaternion.AngleAxis(度数法, 軸);
             head.rotation *= Quaternion.AngleAxis(headRotateSpeed * Time.deltaTime, Vector3.up);
         }
+
+        //バリアのフレーム
+        if (burrierCoolTime <= 0.0f)
+        {
+            burrierCoolTime = 0;
+        }
+        else
+        {
+            burrierCoolTime--;
+        }
+
+        if (isBarrier)
+        {
+            burrierFreme--;
+            if (burrierFreme < 0)
+            {
+                burrierFreme = 200.0f;
+                isBarrier = false;
+                barrier.SetActive(false);
+            }
+        }
         //ビーム発射フレームカウント開始
         if (isBeamCount)
         {
 
             beamGauge = 0;
             beamFreamCount++;
-            if (beamFreamCount > 60)
+            if (beamFreamCount > 50.0f)
             {
+                //プレイヤーの反動
+                playerRb.AddForce(this.transform.forward * -200 * Time.deltaTime, ForceMode.Impulse);
+
                 beamCharge.SetActive(false);
                 maxEffect.SetActive(false);
                 //弾の発射する場所を取得する
@@ -165,10 +220,11 @@ public class SinglePlayer : MonoBehaviour
                 //
                 newBeam = Instantiate(bulletBeam, bulletPosition, head.gameObject.transform.rotation);
 
-
                 beamFreamCount = 0;
                 isBeamCount = false;
                 isShotBeam = false;
+
+
             }
             return;
         }
@@ -179,16 +235,36 @@ public class SinglePlayer : MonoBehaviour
 
         //玉のクールタイム
         //クールタイムカウント
-        coolTime++;
-        if (coolTime > canCoolTime)
+        //if (coolTime > 20)
+        //{
+        //    moveSpeed = tempSpeed;
+        //}
+        nomalCoolTime++;
+        if (nomalCoolTime > canNomalCoolTime)
         {
-            isShotNomal = true;
+            if (!isShotNomal)
+            {
+                //プレイヤーの反動
+                playerRb.AddForce(this.transform.forward * -200 * Time.deltaTime, ForceMode.Impulse);
+                //弾の発射する場所を取得する
+                Vector3 bulletPosition = shotPoint.transform.position;
+                //
+                GameObject newBullet = Instantiate(bulletNomal, bulletPosition, head.gameObject.transform.rotation);
+                Vector3 dir = newBullet.transform.forward;
+                newBullet.GetComponent<Rigidbody>().AddForce(dir * nomalBulletSpeed * Time.deltaTime, ForceMode.Impulse);
+                nomalCoolTime = 0;
+            }
+        }
+        strongCoolTime++;
+        if (strongCoolTime > canStrongCoolTime)
+        {
             isShotStrong = true;
         }
-        if (coolTime > 20)
-        {
-            moveSpeed = tempSpeed;
-        }
+
+
+        //isShotNomal = false;
+        //isShotStrong = true;
+
         //チャージ中は球を打てなくしたい
         //ゲージチャージ
         if (isCharge)
@@ -207,70 +283,92 @@ public class SinglePlayer : MonoBehaviour
                 beamGauge += chargeValue;
                 chargeEffect.SetActive(true);
             }
-            isShotNomal = false;
-            isShotStrong = false;
             return;
         }
+
+
 
         //プレイヤーの移動
         playerRb.AddForce(this.transform.forward * inputMove.y * moveSpeed * Time.deltaTime, ForceMode.Impulse);
         //Quaternion.AngleAxis(度数法, 軸);
         this.transform.rotation *= Quaternion.AngleAxis(inputMove.x * bodyRotateSpeed * Time.deltaTime, Vector3.up);
+
+
     }
 
     public void OnShotNomal(InputAction.CallbackContext context)
     {
-       
-        moveSpeed = 0;
-        if (isShotNomal)
+        
+        if (startCount != null || isBarrier || isCharge)
         {
-            //音(sound1)を鳴らす
-            audioSource.PlayOneShot(seSound[0]);
-            //弾の発射する場所を取得する
-            Vector3 bulletPosition = shotPoint.transform.position;
-            //
-            GameObject newBullet = Instantiate(bulletNomal, bulletPosition, head.gameObject.transform.rotation);
-            Vector3 dir = newBullet.transform.forward;
-            newBullet.GetComponent<Rigidbody>().AddForce(dir * nomalBulletSpeed * Time.deltaTime, ForceMode.Impulse);
-            coolTime = 0;
-            isShotStrong = false;
+            return;
+        }
+        if (context.performed)
+        {
+            moveSpeed = 0;
+            //押した瞬間の処理
             isShotNomal = false;
+        }
+        if (context.canceled)
+        {
+            moveSpeed = tempSpeed;
+            //離した瞬間の処理
+            isShotNomal = true;
         }
     }
     public void OnShotStrong(InputAction.CallbackContext context)
     {
-       
-        moveSpeed = 0;
-        if (isShotStrong)
+      
+        if (startCount != null || isBarrier || isCharge)
         {
+            return;
+        }
+        if (context.started && isShotStrong) // ボタンを押したとき
+        {
+            //プレイヤーの反動
+            playerRb.AddForce(this.transform.forward * -500 * Time.deltaTime, ForceMode.Impulse);
             //音(sound1)を鳴らす
-            audioSource.PlayOneShot(seSound[0]);
+            audioSource.PlayOneShot(seSound);
             //弾の発射する場所を取得する
             Vector3 bulletPosition = shotPoint.transform.position;
             //
             GameObject newBullet = Instantiate(bulletStrong, bulletPosition, head.gameObject.transform.rotation);
             Vector3 dir = newBullet.transform.forward;
             newBullet.GetComponent<Rigidbody>().AddForce(dir * strongBulletSpeed * Time.deltaTime, ForceMode.Impulse);
-            coolTime = 0;
-            isShotNomal = false;
+            strongCoolTime = 0;
             isShotStrong = false;
+        }
+
+    }
+
+    public void OnShotBarrier(InputAction.CallbackContext context)
+    {
+       
+        if (startCount != null)
+        {
+            return;
+        }
+        if (context.performed && burrierCoolTime <= 0.0f && !isBeamCount)
+        {
+            //押した瞬間の処理
+            barrier.SetActive(true);
+            burrierCoolTime = 1800.0f;
+            isBarrier = true;
         }
     }
 
-    public void OnShotBeam(InputAction.CallbackContext context)
+    public void OnChargeAndBeam(InputAction.CallbackContext context)
     {
-        if (context.started && isShotBeam) // ボタンを押したとき
+       
+        if (startCount != null && !isBarrier)
         {
-            //音(sound1)を鳴らす
-            audioSource.PlayOneShot(seSound[1]);
+            return;
+        }
+        if (context.started && isShotBeam && !isBarrier) // ボタンを押したとき
+        {
             beamCharge.SetActive(true);
             isBeamCount = true;
         }
-    }
-
-    public void OnCharge(InputAction.CallbackContext context)
-    {
-      
         if (context.performed)
         {
             //押した瞬間の処理
@@ -291,6 +389,7 @@ public class SinglePlayer : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        
         // 入力値を保持しておく
         inputMove = context.ReadValue<Vector2>();
 
@@ -299,7 +398,8 @@ public class SinglePlayer : MonoBehaviour
 
 
 
+   
+   
+
     
 }
-
-
